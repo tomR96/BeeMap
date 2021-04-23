1a20ca9ad2e9da28399a0351c731986139e25e60
tomR96 <tommyryan12@yahoo.ie>
2021-04-23 16:06:09 +0100

    bee android

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Activity2.java
@@ -0,0 +1,10 @@
+package com.example.application.myapplication;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class Activity2 extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
